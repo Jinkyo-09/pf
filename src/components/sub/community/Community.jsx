6b80ed64ea046691c14c68f0a9a2c5c22b4f07bf
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-13 10:15:28 +0900

    커뮤니티 삭제 기능 구형

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -11,16 +11,20 @@ export default function Community() {
 		refInput.current.value = '';
 		refTextarea.current.value = '';
 	};
+
 	const createPost = () => {
 		if (!refInput.current.value.trim() || !refTextarea.current.value.trim()) {
 			resetForm();
 			return alert('제목과 본문을 모두 입력하세요.');
 		}
 		//기존 Posts 배열값을 Deep copy해서 가져온뒤, 그 뒤에 추가로 방금 입력한 객체를 배열에 추가
-		setPosts([
-			{ title: refInput.current.value, content: refTextarea.current.value },
-			...Posts,
-		]);
+		setPosts([{ title: refInput.current.value, content: refTextarea.current.value }, ...Posts]);
+		resetForm();
+	};
+
+	const deletePost = (delIndex) => {
+		//기존 Posts배열을 반복 돌면서 인수로 전달된 삭제 순번값과 현재 반복되는 배열의 순번값이 같지 않은 것만 리턴
+		setPosts(Posts.filter((_, idx) => delIndex !== idx));
 	};
 
 	return (
@@ -28,12 +32,7 @@ export default function Community() {
 			<div className='inputBox'>
 				<input ref={refInput} type='text' placeholder='제목을 입력하세요.' />
 				<br />
-				<textarea
-					ref={refTextarea}
-					cols='30'
-					rows='3'
-					placeholder='본문을 입력하세요.'
-				></textarea>
+				<textarea ref={refTextarea} cols='30' rows='3' placeholder='본문을 입력하세요.'></textarea>
 
 				<nav className='btnSet'>
 					<button onClick={resetForm}>cancel</button>
@@ -47,6 +46,11 @@ export default function Community() {
 						<article key={idx}>
 							<h2>{post.title}</h2>
 							<p>{post.content}</p>
+
+							<nav className='btnSet'>
+								<button>Edit</button>
+								<button onClick={() => deletePost(idx)}>Delete</button>
+							</nav>
 						</article>
 					);
 				})}
