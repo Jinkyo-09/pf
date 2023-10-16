f0fd15782b14fbc6ecd79480386204954162586c
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-16 09:38:33 +0900

    회원가입 페이지 기본 인풋 요소 입력

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -4,7 +4,44 @@ import './Members.scss';
 export default function Members() {
 	return (
 		<Layout title={'Members'}>
-			<p>회원가입 페이지입니다.</p>
+			<form>
+				<fieldset>
+					<legend>회원가입 폼 양식</legend>
+					<table border='1'>
+						<tbody>
+							{/* userID */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='userid'>userID</label>
+								</th>
+								<td>
+									<input type='text' id='userid' name='userid' />
+								</td>
+							</tr>
+
+							{/* password1 */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pw1'>password</label>
+								</th>
+								<td>
+									<input type='text' id='pw1' name='pw1' />
+								</td>
+							</tr>
+
+							{/* password2 */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pw2'>Password</label>
+								</th>
+								<td>
+									<input type='text' id='pw2' name='pw2' />
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
 		</Layout>
 	);
 }
