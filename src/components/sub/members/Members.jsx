0da31d81df187e4a2fefe2a56f4597feea25fc73
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-16 13:16:58 +0900

    이메일 인증 로직

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -37,6 +37,22 @@ export default function Members() {
 		if (value.pw1 !== value.pw2) {
 			errs.pw2 = '비밀번호가 일치하지 않습니다.';
 		}
+
+		//이메일 인증
+		if (!value.email || !/@/.test(value.email)) {
+			errs.email = '이메일은 무조건 @를 포함해야 합니다.';
+		} else {
+			const [forward, backward] = value.email.split('@');
+			if (!forward || !backward) {
+				errs.email = '이메일에 @앞뒤로 문자값이 있어야 합니다.';
+			} else {
+				const [forward, backward] = value.email.split('.');
+				if (!forward || !backward) {
+					errs.email = '이메일 . 앞뒤로 문자값이 있어야 합니다.';
+				}
+			}
+		}
+
 		return errs;
 	};
 
