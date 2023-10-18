81f3d5897a53485863c32ec51efe10bf5f3e354a
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-18 16:18:35 +0900

    메뉴 모션 깔끔하게

--- src/App.js
+++ src/App.js
@@ -14,12 +14,11 @@ import { useMedia } from './hooks/useMedia';
 import { useEffect } from 'react';
 import { fetchYoutube } from './redux/youtubeSlice';
 import { fetchFlickr } from './redux/flickrSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Menu from './components/common/menu/menu';
 
 function App() {
 	const dispatch = useDispatch();
-	const { isOpen } = useSelector((store) => store.menu);
 
 	useEffect(() => {
 		dispatch(fetchYoutube());
@@ -45,7 +44,7 @@ function App() {
 			<Route path='/contact' component={Contact} />
 			<Route path='/community' component={Community} />
 			<Route path='/detail/:id' component={Detail} />
-			{isOpen && <Menu />}
+			<Menu />
 		</main>
 	);
 }
