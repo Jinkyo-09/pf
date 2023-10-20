7c5894a2ef4e908f7d1064c4c79f2e0fd684c4ee
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-20 15:17:37 +0900

    react-query기반 유튜브 커스텀훅 추가

---
+++ src/hooks/useYoutube.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchYoutube = async () => {
+	const api_key = process.env.REACT_APP_YOUTUBE_API;
+	const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+	const pid = 'PL2pfG9YtKxMJVwlAjAIV9TBF5-44WnQ-q';
+	const num = 5;
+	const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+
+	const { data } = await axios.get(resultURL);
+	return data.items;
+};
+
+/**
+  react-quary는 쿼리키를 문자열로 지정해서 데이터 호출시 쿼리키의 문자열이 동일하면 동일한 데이터로 인지해서 refetching처리하지 않고 캐싱되어있는 데이터를 재활용한다
+
+  useQuary([쿼리키],fetching함수, 캐싱옵션)
+ */
+
+export const useYoutubeQuary = () => {
+	return useQuery(['YoutubeData'], fetchYoutube);
+};
