04705e895393b075cb59141f81df609dbed9e811
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-20 10:51:16 +0900

    footer 스타일링

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,3 +1,44 @@
+import './Footer.scss';
+import { FaTwitter, FaInstagramSquare, FaFacebookSquare } from 'react-icons/fa';
+
 export default function Footer() {
-	return <footer>Footer</footer>;
+	const twtUrl = 'https://twitter.com/';
+	const isgUrl = 'https://www.instagram.com/';
+	const fbkUrl = 'https://www.facebook.com/';
+
+	return (
+		<footer className='footer'>
+			<h1>DCODELAB</h1>
+			<ul>
+				<li>
+					<FaTwitter
+						className='twitter'
+						fontSize={25}
+						color={'#ddd'}
+						onClick={() => {
+							window.open(twtUrl);
+						}}
+					/>
+
+					<FaInstagramSquare
+						className='instagram'
+						fontSize={25}
+						color={'#ddd'}
+						onClick={() => {
+							window.open(isgUrl);
+						}}
+					/>
+
+					<FaFacebookSquare
+						className='facebook'
+						fontSize={25}
+						color={'#ddd'}
+						onClick={() => {
+							window.open(fbkUrl);
+						}}
+					/>
+				</li>
+			</ul>
+		</footer>
+	);
 }
